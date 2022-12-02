c2ff7bc4d3fd8cbaacfd815b76cebbabcae8dcd4
Mervebayindir <ceren_626@hotmail.com>
2022-12-02 15:04:24 +0300

    seventh commit

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
@@ -7,21 +9,33 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent {
+  productAddForm : FormGroup;
+  constructor(
+    private formBuilder:FormBuilder, 
+    private productService:ProductService, 
+    private toastrService:ToastrService){}
 
-  productAddForm: FormGroup;
-
-  constructor(private formBuilder:FormBuilder){}
-
-   ngOnUnit(): void{
+   ngOnInit(): void{
       this.createProductAddForm();
    }
    createProductAddForm(){
     this.productAddForm = this.formBuilder.group({
-      productName:["", Validators.required],
+      productName: ["", Validators.required],
       unitPrice: ["", Validators.required],
-      unitsInStock:["", Validators.required],
-      categoryId:["", Validators.required]
+      unitsInStock: ["", Validators.required],
+      categoryId: ["", Validators.required]
     })
    }
-
+   add(){
+    if (this.productAddForm.valid ) {
+      let productModel = Object.assign({}, this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response=>{
+        console.log(response)
+        this.toastrService.success(response.message, "Başarılı")
+      })
+    }else{
+      this.toastrService.error("Formunuz eksik" ,"Dikatt!!!")
+    }
+   }
+   
 }
