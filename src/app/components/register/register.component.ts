2e3adc45e03ae2642f4a4fff7cf7fdb3dc6fe740
Mervebayindir <ceren_626@hotmail.com>
2022-12-06 03:00:58 +0300

    control 5

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
